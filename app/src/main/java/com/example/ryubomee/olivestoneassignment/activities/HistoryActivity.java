e5f0ff80f99664b792db68f00abde7a782f212eb
RyuBomee <ryubbb94@me.com>
2017-12-05 18:37:32 +0900

    basic layout

---
+++ app/src/main/java/com/example/ryubomee/olivestoneassignment/activities/HistoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.ryubomee.olivestoneassignment.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ryubomee.olivestoneassignment.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
--- app/src/main/java/com/example/ryubomee/olivestoneassignment/activities/MainActivity.java
+++ app/src/main/java/com/example/ryubomee/olivestoneassignment/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.ryubomee.olivestoneassignment;
+package com.example.ryubomee.olivestoneassignment.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.ryubomee.olivestoneassignment.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
